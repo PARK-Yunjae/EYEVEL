561766d4d98e17799419bf2b76b1186fd71a9568
yuniwon <dnjsdndus@gmail.com>
2024-02-23 14:14:59 +0900

    필요없는 파일 삭제

--- EYEVELbackup/src/main/java/com/eyevel/frontController/ViewResolver.java
+++
@@ -1,8 +0,0 @@
-package com.eyevel.frontController;
-
-public class ViewResolver {
-	   public static String makeView(String nextPage) {
-		   return "/WEB-INF/"+nextPage+".jsp";
-	   }
-	}
-
--- MemberMVC6/src/main/java/kr/basic/frontcontroller/ViewResolver.java
+++
@@ -1,6 +0,0 @@
-package kr.basic.frontcontroller;
-public class ViewResolver {
-   public static String makeView(String nextPage) {
-	   return "/WEB-INF/member/"+nextPage+".jsp";
-   }
-}
