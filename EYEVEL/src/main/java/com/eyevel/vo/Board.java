d77a78f2bfe50564cd17a6785b0042882f690807
kyungjae-Y <yoonkj2008@gmail.com>
2024-02-23 15:42:18 +0900

    update table.sql and VO
    update table.sql and VO

--- EYEVEL/src/main/java/com/eyevel/vo/Board.java
+++ EYEVEL/src/main/java/com/eyevel/vo/Board.java
@@ -1,7 +1,8 @@
 package com.eyevel.vo;
+
 //CLS-005 게시판 테이블
 public class Board {
-	
+
 	private int no;
 	private int category;
 	private int isPrivate;
@@ -9,70 +10,95 @@ public class Board {
 	private String title;
 	private String contents;
 	private String regDate;
+	private String hits;
 	private int heart;
 	private int adminCheck;
-	
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
 	public int getCategory() {
 		return category;
 	}
+
 	public void setCategory(int category) {
 		this.category = category;
 	}
+
 	public int getIsPrivate() {
 		return isPrivate;
 	}
+
 	public void setIsPrivate(int isPrivate) {
 		this.isPrivate = isPrivate;
 	}
+
 	public String getMemeberId() {
 		return memeberId;
 	}
+
 	public void setMemeberId(String memeberId) {
 		this.memeberId = memeberId;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContents() {
 		return contents;
 	}
+
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
+
 	public String getRegdate() {
 		return regDate;
 	}
+
 	public void setRegdate(String regdate) {
 		this.regDate = regdate;
 	}
+
 	public int getHeart() {
 		return heart;
 	}
+
 	public void setHeart(int heart) {
 		this.heart = heart;
 	}
+
 	public int getAdminCheck() {
 		return adminCheck;
 	}
+
 	public void setAdminCheck(int adminCheck) {
 		this.adminCheck = adminCheck;
 	}
+
+	public String getHits() {
+		return hits;
+	}
+
+	public void setHits(String hits) {
+		this.hits = hits;
+	}
+
 	@Override
 	public String toString() {
 		return "Board [no=" + no + ", category=" + category + ", isPrivate=" + isPrivate + ", memeberId=" + memeberId
-				+ ", title=" + title + ", contents=" + contents + ", regdate=" + regDate + ", heart=" + heart
-				+ ", adminCheck=" + adminCheck + "]";
+				+ ", title=" + title + ", contents=" + contents + ", regDate=" + regDate + ", hits=" + hits + ", heart="
+				+ heart + ", adminCheck=" + adminCheck + "]";
 	}
-	
-	
 
 }
