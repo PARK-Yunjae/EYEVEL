0a091ab4b91e0dca9c372f9413b978f5f2a1a0ec
kyungjae-Y <yoonkj2008@gmail.com>
2024-02-26 14:14:40 +0900

    Update FAQListController.java

--- EYEVEL/src/main/java/com/eyevel/controller/board/FAQListController.java
+++ EYEVEL/src/main/java/com/eyevel/controller/board/FAQListController.java
@@ -15,7 +15,7 @@ public class FAQListController implements Controller {
 	public String requestHandler(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
 		
-		return null;
+		return "eyevel/board/faqList";
 	}
 
 }
