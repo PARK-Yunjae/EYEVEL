858e6aa11fec21cdc4c2ba4fe323ff1986f537ee
yuniwon <dnjsdndus@gmail.com>
2024-02-28 14:02:24 +0900

    날씨표시 관련 수정

--- EYEVEL/src/main/webapp/WEB-INF/eyevel/area/southAmerica.jsp
+++ EYEVEL/src/main/webapp/WEB-INF/eyevel/area/southAmerica.jsp
@@ -1,5 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ include file="../parts/header.jsp"%>
-남아메이카 입니다
+<link rel="stylesheet" type="text/css" href="${ctx}/css/earth.css">
+<section class="earthSection">
+	<div class="innerBox">
+		<button id="Antarctica" onclick="location.href='${ctx}/areaCity.do?no=18&loginId=${loginId}'">남극</button>
+		<img src="${ctx}/img/worldmap1.png"/>
+	</div>
+</section>
 <%@ include file="../parts/footer.jsp"%>
--- EYEVEL/src/main/webapp/js/weather.js
+++ EYEVEL/src/main/webapp/js/weather.js
@@ -1,7 +1,7 @@
 //api-key 
 const apiKey = "ed670c939f8e6b9849a9eb86bf91bf21";
 // city
-let city = "Seoul";
+let city = "seoul";
 let cityname = "서울";
 //날씨 api - fontawesome 아이콘
 var weatherIcon = {
@@ -28,23 +28,14 @@ $.ajax({
         var $Icon = (resp.weather[0].icon).substr(0,2);
         var $weather_description = resp.weather[0].main;
         var $Temp = Math.floor(resp.main.temp- 273.15) + 'º';
-        var $humidity = '습도&nbsp;&nbsp;&nbsp;&nbsp;' + resp.main.humidity+ ' %';
-        var $wind = '바람&nbsp;&nbsp;&nbsp;&nbsp;' +resp.wind.speed + ' m/s';
         var $city = cityname;
-        var $cloud = '구름&nbsp;&nbsp;&nbsp;&nbsp;' + resp.clouds.all +"%";
-        var $temp_min = '최저 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_min- 273.15) + 'º';
-        var $temp_max = '최고 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_max- 273.15) + 'º';
-        
 
-        $('.weather_icon').append('<i class="' + weatherIcon[$Icon] +' fa-5x" style="height : 150px; width : 150px;"></i>');
+        $('.weather_icon').append('<i class="' + weatherIcon[$Icon] +' fa-5x" style="height : 30px; width : 30px;"></i>');
         $('.weather_description').prepend($weather_description);
-        $('.current_temp').prepend($Temp);
-        $('.humidity').prepend($humidity);
-        $('.wind').prepend($wind);
-        $('.city').append($city);
-        $('.cloud').append($cloud);
-        $('.temp_min').append($temp_min);
-        $('.temp_max').append($temp_max);               
+        $('.weather_description').prepend($Temp);
+        $('.weather_description').prepend($city);
+/*        $('.current_temp').prepend($Temp);
+        $('.city').append($city);   */          
     }
 }) 
 
